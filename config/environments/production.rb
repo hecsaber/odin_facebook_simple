6e2f9540a398b9d025c2479abd69647ea00ea008
Csaba Hegyi <hegy.csab@gmail.com>
2021-01-22 07:59:06 +0100

    moved smtp to env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,13 +112,4 @@ Rails.application.configure do
 
   config.active_storage.service = :amazon
 
-  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['apikey'],
-    :password => ENV['your_sendgrid_api_key'],
-    :domain => ENV['yourdomain.com'],
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
 end
